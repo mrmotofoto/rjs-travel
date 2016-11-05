cd45e858a0933e015dbcafa8bea347d781a888bf
Rich <richardstiehm@gmail.com>
2016-11-05 15:00:31 -0400

    Added Img and Centered Text

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
+var cssimport = require('postcss-import');
+
 gulp.task('default', function(){
 	console.log('Gulp Task');
 });
@@ -14,7 +16,7 @@ gulp.task('html', function(){
 
 gulp.task('styles', function(){
 	return gulp.src('./app/assets/styles/styles.css')
-		   .pipe(postcss([nested, cssvars, autoprefixer]))
+		   .pipe(postcss([cssimport, nested, cssvars, autoprefixer]))
 		   .pipe(gulp.dest('./app/temp/styles'));
 });
 
